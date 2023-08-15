02e8a5a8cc351b2d09d5362db45e54012a3a234c
WellyAguiar <wellyagui@gmail.com>
2023-08-15 10:49:29 -0300

    commit inicial

---
+++ aula3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int nota1, nota2 ,nota3 ,media;
+    printf("Digite 3 notas:\n")
+
+    printf("Nota1: ")
+    scanf("%d", &nota1);
+
+    printf("Nota2: ")
+    scanf("%d", &nota2);
+
+    printf("Nota3: ")
+    scanf("%d", &nota3);
+
+    media = (nota1 + nota2 + nota3)/3
+
+    
+
+
+}
